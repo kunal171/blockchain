f3146810bf8bafcbc476fcab444228106c039cae
kunal171 <0xshadyr6@gmail.com>
2026-07-11 00:29:14 +0530

    feat: define Message enum for gossip network communication

--- src/gossip/message.rs
+++ src/gossip/message.rs
@@ -4,3 +4,16 @@
 use crate::gossip::peer::{Peer, PeerId};
 
 /// Every message exchanged over the gossip network.
+pub enum Message {
+    Join { id: PeerId, addr: String }, // A new peer is joining the network
+
+    PeerList { peers: Vec<Peer> }, // A list of known peers
+
+    Gossip {Payload: String}, // A gossip message with a payload
+
+    Ping { id: PeerId }, // A ping message to check if a peer is alive
+
+    Ack { id: PeerId }, // An acknowledgment message in response to a ping
+
+    PingRequest { id: PeerId }, // A request to ping a peer
+}
